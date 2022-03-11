fcab649ecfd2e9a56faeae708f0fc1d9ea17910d
ThanhTrangPhan <phanthanh.trang200@gmail.com>
2022-03-11 13:36:26 +0700

    init

---
+++ pancake-swap-core-v1/hardhat.config.js
@@ -0,0 +1,36 @@
+require ("@nomiclabs/hardhat-ethers");
+require ("@nomiclabs/hardhat-waffle");
+const {privateKey,feeToSetter} = require("./env");
+module.exports = {
+  defaultNetwork: "testnet",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: privateKey
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000
+    }
+  },
+  solidity: {
+  version: "0.5.16",
+  settings: {
+    optimizer: {
+      enabled: true
+    }
+   }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
